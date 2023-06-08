1684ac80c1b65a83fd0b819e269d5cc9464b4d95
Bobby R. Bruce <bbruce@ucdavis.edu>
2023-06-08 06:01:54 -0700

    Update version to specifc ssbse-challenge track

--- src/base/version.cc
+++ src/base/version.cc
@@ -32,6 +32,6 @@ namespace gem5
 /**
  * @ingroup api_base_utils
  */
-const char *gem5Version = "[DEVELOP-FOR-23.0]";
+const char *gem5Version = "[SSBSE Challenge Track '23 Variant]";
 
 } // namespace gem5
